dd70692dab2beedc81246c5631b99a3bc802db78
chenqixian <chenqixian@buaa.edu.cn>
2019-11-22 15:04:26 +0800

    calculator fucntion

--- pages/calc/calc.js
+++ pages/calc/calc.js
@@ -7,7 +7,6 @@ Page({
   data: {
     idb: "back",
     idc: "clear",
-    idt: "toggle",
     idadd: "＋",
     id9: "9",
     id8: "8",
@@ -26,7 +25,11 @@ Page({
     ide: "＝",
     screenData: "0",
     iconType: 'waiting_circle',
-    iconColor: 'white'
+    iconColor: 'white',
+    arr: [],
+    logs:[],
+    lastIsOperaSymbo: false,
+    operaSymbo: { "＋": "+", "－": "-", "×": "*", "÷": "/", ".": "." }
   },
 
   /**
@@ -85,6 +88,107 @@ Page({
 
   },
   clickBtn: function(event) {
+    let id = event.target.id;
+    // delete the last number
+    if(id == this.data.idb) {
+      let data = this.data.screenData;
+      if(data == "0") {
+        return;
+      }
+      data = data.substring(0, data.length - 1);
+      if(data == "" || data == "-") {
+        data = 0;
+      }
+      this.setData({"screenData": data});
+      this.data.arr.pop();
+    }
+    // clear value
+    else if(id == this.data.idc) {
+      this.setData({"screenData": "0"});
+      this.data.arr.length = 0;
+    }
+    // calculate equals
+    else if(id == this.data.ide) {
+      let data = this.data.screenData;
+      if(data == "0") {
+        return;
+      }
+      let lastWord = data.charAt(data.length);
+      if(isNaN(lastWord)) {
+        return;
+      }
+      let num = "";
+      let lastOperator = "";
+      let arr = this.data.arr;
+      let optarr = [];
+      for(let i in arr) {
+        if(isNaN(arr[i]) == false || arr[i] == this.data.idd) {
+          num += arr[i];
+        }
+        else {
+          lastOperator = arr[i];
+          optarr.push(num);
+          optarr.push(arr[i]);
+          num = "";
+        }
+      }
+      optarr.push(Number(num));
+      let result = Number(optarr[0]) * 1.0;
+      console.log(result);
+      for(let i = 1 ; i < optarr.length ; i ++) {
+        if(isNaN(optarr[i])) {
+          if(optarr[1] == this.data.idadd) {
+            result += Number(optarr[i + 1]);
+          }
+          else if(optarr[1] == this.data.idj) {
+            result -= Number(optarr[i + 1]);
+          }
+          else if(optarr[1] == this.data.idx) {
+            result *= Number(optarr[i + 1]);
+          }
+          else if(optarr[1] == this.data.iddiv) {
+            result /= Number(optarr[i + 1]);
+          }
+        }
+      }
+      // save history log
+      this.data.logs.push(data + result);
+      wx.setStorageSync("calclogs", this.data.logs);
 
+      this.data.arr.length = 0;
+      this.data.arr.push(result);
+
+      this.setData({"screenData": result+""});
+    }
+    // input value
+    else {
+      if(this.data.operaSymbo[id]) {
+        if(this.data.lastIsOperaSymbo || this.data.screenData == "0") {
+          return;
+        }
+      }
+      let sd = this.data.screenData;
+      let data;
+      if(sd == 0) {
+        data = id;
+      }
+      else {
+        data = sd + id;
+      }
+      this.setData({"screenData": data});
+      this.data.arr.push(id);
+      if(this.data.operaSymbo[id]) {
+        this.setData({"lastIsOperaSymbo": true});
+      }
+      else {
+        this.setData({ "lastIsOperaSymbo": false });
+      }
+    }
+  },
+  history: function() {
+    wx.navigateTo({
+      url: '../history/history'
+    })
   }
+
 })
