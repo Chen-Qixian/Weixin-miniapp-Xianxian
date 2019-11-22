577465c64cee910da99fd4f8a8b1946f88a86dfe
chenqixian <chenqixian@buaa.edu.cn>
2019-11-22 16:08:15 +0800

    bug fixed

--- pages/calc/calc.js
+++ pages/calc/calc.js
@@ -113,46 +113,75 @@ Page({
       if(data == "0") {
         return;
       }
-      let lastWord = data.charAt(data.length);
+      let lastWord = data.charAt(data.length - 1);
       if(isNaN(lastWord)) {
         return;
       }
       let num = "";
-      let lastOperator = "";
       let arr = this.data.arr;
       let optarr = [];
+      let optstack = [];
+      let opthead = -1;
+      let numstack = [];
+      let numhead = -1;
       for(let i in arr) {
         if(isNaN(arr[i]) == false || arr[i] == this.data.idd) {
           num += arr[i];
         }
         else {
-          lastOperator = arr[i];
           optarr.push(num);
           optarr.push(arr[i]);
           num = "";
         }
       }
       optarr.push(Number(num));
-      let result = Number(optarr[0]) * 1.0;
-      console.log(result);
-      for(let i = 1 ; i < optarr.length ; i ++) {
+      /**Error calculation! */
+      // let result = Number(optarr[0]) * 1.0;
+      // for(let i = 1 ; i < optarr.length ; i ++) {
+      //   if(isNaN(optarr[i])) {
+      //     if(optarr[1] == this.data.idadd) {
+      //       result += Number(optarr[i + 1]);
+      //     }
+      //     else if(optarr[1] == this.data.idj) {
+      //       result -= Number(optarr[i + 1]);
+      //     }
+      //     else if(optarr[1] == this.data.idx) {
+      //       result *= Number(optarr[i + 1]);
+      //     }
+      //     else if(optarr[1] == this.data.iddiv) {
+      //       result /= Number(optarr[i + 1]);
+      //     }
+      //   }
+      // }
+      /**Modify calculation! */
+      for(let i = 0 ,len = optarr.length; i < len ; i ++) {
         if(isNaN(optarr[i])) {
-          if(optarr[1] == this.data.idadd) {
-            result += Number(optarr[i + 1]);
+          if(optarr[i] == this.data.idadd || optarr[i] == this.data.idj) {
+            optstack[++ opthead] = optarr[i];
           }
-          else if(optarr[1] == this.data.idj) {
-            result -= Number(optarr[i + 1]);
+          else if(optarr[i] == this.data.idx){
+            numstack[numhead] *= optarr[++i];
           }
-          else if(optarr[1] == this.data.idx) {
-            result *= Number(optarr[i + 1]);
-          }
-          else if(optarr[1] == this.data.iddiv) {
-            result /= Number(optarr[i + 1]);
+          else if(optarr[i] == this.data.iddiv) {
+            numstack[numhead] /= optarr[++i];
           }
         }
+        else {
+          numstack[++ numhead] = Number(optarr[i]);
+        }
       }
+      let result = Number(numstack[0]) * 1.0;
+      for(let i = 0 ,len = optstack.length ; i < len ; i ++) {
+        if(optstack[i] == this.data.idadd) {
+          result += Number(numstack[i+1]);
+        }
+        else if(optstack[i] == this.data.idj) {
+          result -= Number(numstack[i+1]);
+        }
+      }
+      result.toFixed(0);
       // save history log
-      this.data.logs.push(data + result);
+      this.data.logs.push(data + "=" + result);
       wx.setStorageSync("calclogs", this.data.logs);
 
       this.data.arr.length = 0;
@@ -162,6 +191,9 @@ Page({
     }
     // input value
     else {
+      if(this.data.screenData.length > 8) {
+        return;
+      }
       if(this.data.operaSymbo[id]) {
         if(this.data.lastIsOperaSymbo || this.data.screenData == "0") {
           return;
