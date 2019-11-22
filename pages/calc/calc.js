b73b79dc4e1ae823a4e7f4c51de35dd0fe8acb8d
chenqixian <chenqixian@buaa.edu.cn>
2019-11-22 14:20:22 +0800

    初始化计算器布局

---
+++ pages/calc/calc.js
@@ -0,0 +1,90 @@
+// pages/calc/calc.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+    idb: "back",
+    idc: "clear",
+    idt: "toggle",
+    idadd: "＋",
+    id9: "9",
+    id8: "8",
+    id7: "7",
+    idj: "－",
+    id6: "6",
+    id5: "5",
+    id4: "4",
+    idx: "×",
+    id3: "3",
+    id2: "2",
+    id1: "1",
+    iddiv: "÷",
+    id0: "0",
+    idd: ".",
+    ide: "＝",
+    screenData: "0",
+    iconType: 'waiting_circle',
+    iconColor: 'white'
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  },
+  clickBtn: function(event) {
+
+  }
+})
---
+++ pages/history/history.js
@@ -0,0 +1,67 @@
+// pages/history/history.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+    logs: []
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+    let logs = wx.getStorageSync('calclogs');
+    this.setData({"logs": logs});
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
